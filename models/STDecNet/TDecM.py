b5d3eef8761e867debfd1a3b42f899dbc01c28cb
greekinRoma <zz8843157@gmail.com>
2025-06-26 21:28:19 +0800

    finish

--- models/STDecNet/TDecM.py
+++ models/STDecNet/TDecM.py
@@ -17,13 +17,14 @@ class TDecM(nn.Module):
                                       nn.Conv2d(in_channels=mid_channels,out_channels=1,kernel_size=1,stride=1))
         self.relu = torch.nn.functional.relu
         self.avg_pool = nn.AvgPool2d(kernel_size=3,stride=1,padding=1)
-        self.loc_conv = nn.Sequential(nn.Conv2d(kernel_size=1,stride=1,in_channels=mid_channels,out_channels=mid_channels),
-                                      nn.Conv2d(kernel_size=1,stride=1,in_channels=mid_channels,out_channels=mid_channels))
+        self.loc_conv = nn.Sequential(
+            nn.Conv2d(kernel_size=1,stride=1,in_channels=mid_channels,out_channels=mid_channels),
+            nn.Conv2d(kernel_size=1,stride=1,in_channels=mid_channels,out_channels=mid_channels))
     def forward(self,inp):
         b,c,_,h,w = inp.shape
         origin = self.origin(inp.view(b,-1,h,w)).view(b,self.mid_channels,1,h*w)
-        basis = [inp[:,:,-1]-inp[:,:,0],inp[:,:,-1]-inp[:,:,1],inp[:,:,-1]-inp[:,:,2],inp[:,:,-1]-inp[:,:,3]]
-        basis = torch.stack(basis,dim=2).view(b,self.mid_channels,self.num_frames-1,h*w)
+        basis = [inp[:,:,-1]-inp[:,:,0],inp[:,:,-1]-inp[:,:,1],inp[:,:,-1]-inp[:,:,2],inp[:,:,-1]-inp[:,:,3],inp[:,:,-1]-(inp[:,:,0]+inp[:,:,1]+inp[:,:,2]+inp[:,:,3])/4]
+        basis = torch.stack(basis,dim=2).view(b,self.mid_channels,self.num_frames,h*w)
         basis1 = torch.nn.functional.normalize(basis,dim=-1)
         basis2 = basis1.transpose(-2,-1)
         weight_score = torch.matmul(origin,basis2)
