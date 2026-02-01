b437c519a7be6c6da117312d0077ef01c9dd051f
greekinRoma <zz8843157@gmail.com>
2026-02-02 00:16:54 +0800

    update_files

--- update.sh
+++ update.sh
@@ -4,6 +4,10 @@ unset all_proxy
 unset HTTP_PROXY
 unset HTTPS_PROXY
 unset ALL_PROXY
+unset http_proxy
+unset ftp_proxy
+unset rsync_proxy
+unset no_proxy
 git add *
 git commit -m update_files
 git push origin master
